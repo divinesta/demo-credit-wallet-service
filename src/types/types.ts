23068e6d1547e1ec79516e20ee0c4ade4d95c236
Emilola Divine <emiloladivine123@gmail.com>
2026-06-02 21:09:12 +0100

    feat: add types for User, CreateUserInput, Wallet, TransactionType, TransactionStatus, and WalletTransaction

---
+++ src/types/types.ts
@@ -0,0 +1,44 @@
+export type User = {
+   id: number;
+   firstName: string;
+   lastName: string;
+   email: string;
+   phone: string;
+   bvn: string;
+   createdAt: Date;
+   updatedAt: Date;
+};
+
+export type CreateUserInput = {
+   firstName: string;
+   lastName: string;
+   email: string;
+   phone: string;
+   bvn: string;
+};
+
+export type Wallet = {
+   id: number;
+   userId: number;
+   balance: number;
+   createdAt: Date;
+   updatedAt: Date;
+};
+
+export type TransactionType = "funding" | "withdrawal" | "transfer_debit" | "transfer_credit";
+
+export type TransactionStatus = "successful" | "failed";
+
+export type WalletTransaction = {
+   id: number;
+   walletId: number;
+   relatedWalletId: number | null;
+   type: TransactionType;
+   amount: number;
+   reference: string;
+   status: TransactionStatus;
+   narration: string | null;
+   createdAt: Date;
+   updatedAt: Date;
+};
+
